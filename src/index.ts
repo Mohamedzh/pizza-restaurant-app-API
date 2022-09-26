72ea065cb2eb91510c5d8d6e8fb10d02b2ad0ccf
Mohamed Zahran <mohamed.zahran1@live.com>
2022-09-26 22:31:45 +0200

    bug fix

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { json, urlencoded } from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import "reflect-metadata";
+import AppDataSource from './data-source';
+import productRouter from './routes/products'
+import orderRouter from './routes/orders'
+import categoriesRouter from './routes/categories'
+
+const app = express();
+
+config();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use('/product', productRouter)
+// app.use('/user', userRouter)
+app.use('/order', orderRouter)
+app.use('/category', (categoriesRouter))
+
+
+app.listen(process.env.port, async () => {
+    console.log(`listening on port ${process.env.port}`)
+    try {
+        await AppDataSource.initialize(),
+            console.log('DB connection established')
+    } catch (error) {
+        throw new Error(`error occured ${error as Error}`)
+    }
+}
+)
+
+// module.exports = app;
