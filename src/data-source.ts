6195ce86cf1d5c9d924679cb7dbcb9464699e91f
Mohamed Zahran <mohamed.zahran1@live.com>
2024-02-03 14:45:46 +0200

    fixed typeorm issue and changed supabase connection params

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,19 +6,24 @@ import { OrderLine } from "./Entities/orderLine";
 import { Category } from "./Entities/category";
 // import { User } from "./Entities/user";
 
-config()
+config();
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.pghost,
-    port: Number(process.env.pgport),
-    username: process.env.pguser,
-    password: process.env.pgpassword,
-    database: process.env.database,
-    synchronize: true,
-    logging: false,
-    entities: [Product, Order, OrderLine, Category],
-    migrations: ["migration/*.ts"],
-    subscribers: [],
-})
+  type: "postgres",
+  host: process.env.pghost,
+  port: Number(process.env.pgport),
+  username: process.env.pguser,
+  password: process.env.pgpassword,
+  database: process.env.database,
+  synchronize: true,
+  logging: false,
+  entities: [Product, Order, OrderLine, Category],
+  migrations: ["migration/*.ts"],
+  subscribers: [],
+});
+
+// Product.useDataSource(AppDataSource);
+// Order.useDataSource(AppDataSource);
+// OrderLine.useDataSource(AppDataSource);
+// Category.useDataSource(AppDataSource);
 
 export default AppDataSource;
